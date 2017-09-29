8b567b219f4876ce3297d134f035e486de4c029c
nateUX <nateUX@users.noreply.github.com>
2017-09-28 20:08:49 -0500

    cleaning up js for toc

--- app/js/toc.js
+++ app/js/toc.js
@@ -9,21 +9,10 @@ function buildTOC(){
             var li = document.createElement("li");
             var link = document.createElement("a");
 
-            // link.name = h2s[i].id;
             link.href = "#" + h2s[i].id;
             
             link.innerHTML = h2s[i].innerHTML;
-            // link.addEventListener(
-            //     "click", 
-            //     function scrollToAnchor(e) {
-            //             e.preventDefault();
-            //             var elements = document.getElementsByClassName('content');
-            //             var contentElement = elements[0];
 
-            //             var element = document.getElementById(e.target.name);
-            //             var scrollPos = element.offsetTop - 72;
-            //             window.scroll({ top: scrollPos, left: 0, behavior: 'smooth' });
-            //     })
             li.appendChild(link);
             ul.appendChild(li);            
             }
